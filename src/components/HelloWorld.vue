fdb1e5e317878f9374792d476d3cc775151005fa
Sumer Patel <sumerdpatel@gmail.com>
2020-09-13 13:27:20 +0530

    initial commit with dist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container class="flex align-center justify-center flex-column">
+    <v-row class="text-center">
+
+      <v-col>
+        <h1 class="font-weight-bold mb-3">
+          Let's Calculate your BMI
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Please Enter your Height(mts) & Weight(kg)
+        </p>
+
+        <v-text-field
+            v-model="height"
+            label="Height(M)"
+            :rules="[numberRule]"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="weight"
+            label="Weight(Kg)"
+            :rules="[numberRule]"
+            required
+          ></v-text-field>
+
+          <v-card-actions v-on:click="calBMI">
+          <v-btn
+            text
+            color="deep-purple accent-4"
+          >
+            Calculate
+          </v-btn>
+        </v-card-actions>
+
+        <h1 v-if="result">{{ result }} kg/m<sup>2</sup></h1>
+        <p>{{ weigh }}</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        height: '',
+        weight: '',
+        result: '',
+        weigh: '',
+        numberRule: v  => {
+          if (!v.trim()) return true;
+          if (!isNaN(parseFloat(v)) && v >= 0 && v <= 444) return true;
+          return 'Number has to be between 0 and 444';
+        },
+      }
+    },
+    methods: {
+      calBMI() {
+        let result = this.weight / Math.pow(this.height, 2);
+        this.result = +result.toFixed(2);
+        if (this.result < 18.5) {
+          this.weigh = 'Underweight';
+        } else if (18.51 < this.result && this.result < 24.99) {
+          this.weigh = 'Normal';
+        } else if (25 < this.result && this.result < 29.99) {
+          this.weigh = 'Overweight';
+        } else if (30 < this.result){
+          this.weigh = 'Obesity';
+        } else {
+          this.weigh = 'Please enter proper values!';
+        }
+      }
+    }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
